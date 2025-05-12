6dba929467824247eece378b1fb66cdf1dc9ed46
Dat240906 <phungthanhdat001@gmail.com>
2025-05-12 16:38:25 +0700

    upload

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+export default function App() {
+  return (
+    <div className="flex h-screen flex-col items-center justify-center bg-gray-100">
+      <h1 className="mb-4 text-4xl font-bold text-blue-500">
+        Chúc mừng bạn đã chạy thành công dự án!
+      </h1>
+      <p className="text-lg text-gray-700">
+        Đây là ứng dụng Electron kết hợp với React và Tailwind CSS.
+      </p>
+    </div>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}', // Quét các file trong thư mục src
+    './index.html', // Nếu có file index.html
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
